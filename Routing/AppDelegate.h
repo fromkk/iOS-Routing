3e946b7709154f365a0481898934f79059619116
Kazuya Ueoka <k-ueoka@atgs.jp>
2013-07-30 08:59:15 +0900

    Initial Commit

---
+++ Routing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Routing
+//
+//  Created by Kazuya Ueoka on 2013/07/30.
+//  Copyright (c) 2013年 Kazuya Ueoka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Routing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Routing
+//
+//  Created by Kazuya Ueoka on 2013/07/30.
+//  Copyright (c) 2013年 Kazuya Ueoka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
